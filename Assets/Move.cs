eda56cf9bf1053d36a96c83b2af23e590e0fef26
Kriti Ravindran <kriti.ravindran@gmail.com>
2018-10-01 17:14:05 -0400

    updated files from desktop

---
+++ Assets/Move.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = Camera.main.transform.forward;
+		float scalor = 1f + Time.deltaTime;
+		transform.Translate (direction * scalor);
+	}
+}
